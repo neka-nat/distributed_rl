0642534eefaa9e50d60f19946f4a14d6075562c9
nekanat <nekanat.stock@gmail.com>
2018-10-27 18:40:29 +0900

    score log

--- actor.py
+++ actor.py
@@ -18,8 +18,9 @@ vis = visdom.Visdom()
 class Actor(object):
     EPS_START = 1.0
     EPS_END = 0.1
-    def __init__(self, hostname='localhost', batch_size=32, target_update=50, eps_decay=50000):
+    def __init__(self, name, hostname='localhost', batch_size=32, target_update=50, eps_decay=50000):
         self._env = gym.make('MultiFrameBreakout-v0')
+        self._name = name
         self._batch_size = batch_size
         self._target_update = target_update
         self._eps_decay = eps_decay
@@ -27,12 +28,15 @@ class Actor(object):
         self._target_net = models.DuelingDQN(self._env.action_space.n).to(device)
         self._target_net.eval()
         self._win1 = vis.image(utils.preprocess(self._env.env._get_image()))
+        self._win2 = vis.line(X=np.array([0]), Y=np.array([0.0]),
+                              opts=dict(title='Score %s' % self._name))
         self._local_memory = replay_memory.ReplayMemory(5000)
         self._connect = redis.StrictRedis(host=hostname)
 
     def run(self):
         state = self._env.reset()
         sum_rwd = 0
+        n_episode = 0
         for t in count():
             # Select and perform an action
             eps = self.EPS_END + (self.EPS_START - self.EPS_END) * np.exp(-1. * t / self._eps_decay)
@@ -49,6 +53,8 @@ class Actor(object):
             if done:
                 state = self._env.reset()
                 sum_rwd = 0
+                n_episode += 1
+                vis.line(X=np.array([n_episode]), Y=np.array([sum_rwd]), win=self._win2, update='append')
             if len(self._local_memory) > self._batch_size:
                 samples = self._local_memory.sample(self._batch_size)
                 _, prio = self._policy_net.calc_priorities(self._target_net, samples,
@@ -63,5 +69,9 @@ class Actor(object):
                     self._target_net.load_state_dict(self._policy_net.state_dict())
 
 if __name__ == '__main__':
-    actor = Actor()
+    import argparse
+    parser = argparse.ArgumentParser(description='Actor for distributed reinforcement.')
+    parser.add_argument('-n', '--name', type=str, default='actor1', help='Actor name.')
+    args = parser.parse_args()
+    actor = Actor(args.name)
     actor.run()
