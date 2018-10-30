0ee9c6c2ec79aa4d0e160496540c2aafba48bf37
nekanat <nekanat.stock@gmail.com>
2018-10-30 23:30:12 +0900

    add configs

--- run.sh
+++ run.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+config=${2:-"config/all.conf"}
+source $config
+
+echo "redis server:" $redis_server
+echo "visdom server:" $visdom_server
 
 pids=""
-python -m visdom.server -logging_level WARNING &
-pids="$pids $!"
 
-sleep 1
-
-for i in `seq $1`
-do
-    python actor.py -n actor_$i &
+if $visdom; then
+    python -m visdom.server -logging_level WARNING &
     pids="$pids $!"
-done
+    sleep 1
+fi
 
-python learner.py &
-pids="$pids $!"
+if $actor; then
+    for i in `seq $1`
+    do
+	python actor.py -n actor_$i -r $redis_server -v $visdom_server &
+	pids="$pids $!"
+    done
+fi
+
+if $leaner; then
+    python learner.py -r $redis_server -v $visdom_server &
+    pids="$pids $!"
+fi
 
 wait -n $pids
