f4b06db3025d4bfc82b9f383857fbadf5fb85289
nekanat <nekanat.stock@gmail.com>
2018-11-14 18:25:55 +0900

    add directory arg

--- fabfile.py
+++ fabfile.py
@@ -2,19 +2,19 @@
 from fabric.api import run, cd, settings
 from fabric.api import env
 
-def all_run(num_proc='4'):
-    with cd('distributed_rl'):
+def all_run(directory='distributed_rl', num_proc='4'):
+    with cd(directory):
         with settings(warn_only=True):
             run("./run.sh %s" % num_proc)
 
-def actor_run(num_proc='1', learner_host='localhost'):
+def actor_run(directory='distributed_rl', num_proc='1', learner_host='localhost'):
     print('num_proc: %s' % num_proc)
     print('learner_host: %s' % learner_host)
-    with cd('distributed_rl'):
+    with cd(directory):
         with settings(warn_only=True):
             run("./run.sh %s config/actor.conf %s" % (num_proc, learner_host))
 
-def learner_run():
-    with cd('distributed_rl'):
+def learner_run(directory='distributed_rl'):
+    with cd(directory):
         with settings(warn_only=True):
             run("./run.sh 0 config/learner.conf localhost")
