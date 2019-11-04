2cd97fd539e7eeb6a91009c1374c814cc0881a30
nekanat <nekanat.stock@gmail.com>
2019-11-04 11:18:23 +0900

    bug fix

--- distributed_rl/r2d2/actor.py
+++ distributed_rl/r2d2/actor.py
@@ -52,9 +52,9 @@ class Actor(actor.Actor):
                                                         step_buffer[0].action, r_nstep, done=done))
             if len(sequence_buffer) == n_total_sequence:
                 self._local_memory.push(utils.Sequence(sequence_buffer,
-                                                       recurrent_state_buffer[-n_total_sequence]))
+                                                       recurrent_state_buffer[-(n_total_sequence + step_buffer.maxlen - 1)]))
                 sequence_buffer = sequence_buffer[-n_total_overlap:] if n_total_overlap > 0 else []
-                recurrent_state_buffer = recurrent_state_buffer[-n_total_sequence:]
+                recurrent_state_buffer = recurrent_state_buffer[-(n_total_overlap + step_buffer.maxlen - 1):] if n_total_overlap + step_buffer.maxlen - 1 > 0 else []
             self._vis.image(utils.preprocess(self._env.env._get_image()), win=self._win1)
             state = next_state.copy()
             if done:
